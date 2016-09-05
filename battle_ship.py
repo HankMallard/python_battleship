db856dc5e144c20c91462cd202370726d351401f
HankMallard <howardl01@live.ca>
2016-09-05 15:54:55 -0400

    Added 4 turn per game and minor refactoring

--- battle_ship.py
+++ battle_ship.py
@@ -1,15 +1,15 @@
 from random import randint
 # Setting up a 5 by 5 board
 board = []
-for i in range(5):
-    i = ["O"] * 5
-    board.append(i)
+for x in range(5):
+    board.append(["O"] * 5)
 
 # Printing out the board
 def print_board(board):
     for row in board:
-        print row
+        print (row)
 
+print ("Let's Play Battleship!")
 print_board(board)
 
 # Creating a battleship at a random location
@@ -28,14 +28,19 @@ guess_row = (input("Guess Row:"))
 guess_col = (input("Guess Col:"))
 
 # Includes all possible outcome of the game
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
-else:
-    if guess_row not in range(5) or guess_col not in range(5):
-        print "Oops! You've gone too far!"
-    elif(board[guess_col][guess_col] == "X"):
-        print "You've been here before! Move on already!"
+for turn in range(4): # Added 4 turn per game!
+    print ("Turn", turn + 1)
+    if guess_row == ship_row and guess_col == ship_col:
+        print ("Congratulations! You sunk my battleship!")
+        break
     else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
-        print_board(board)
+        if guess_row not in range(5) or guess_col not in range(5):
+            print ("Oops! You've gone too far!")
+        elif(board[guess_col][guess_col] == "X"):
+            print ("You've been there before! Move on please!")
+        else:
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+            if turn == 3:
+                print ("Game Over!")
