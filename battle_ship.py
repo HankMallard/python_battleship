f34cc3c7861e95d2cef557805618cdc312d64d16
HankMallard <howardl01@live.ca>
2016-09-05 14:08:26 -0400

    Setting up battleship location and User input

--- battle_ship.py
+++ battle_ship.py
@@ -1,3 +1,4 @@
+from random import randint
 # Setting up a 5 by 5 board
 board = []
 for i in range(5):
@@ -8,3 +9,20 @@ for i in range(5):
 def print_board(board):
     for row in board:
         print row
+
+print_board(board)
+
+# Creating a battleship at a random location
+def random_row(board):
+    return randint(0, len(board)-1)
+
+def random_col(board):
+    return randint(0, len(board)-1)
+
+#actual location of the battleship
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+#User input on the location of the battleship
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:")) 
